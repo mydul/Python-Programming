1cee462a2aecc9471335c121a7172d9def35fb63
Mydul Islam Rashed <muhurtoo@gmail.com>
2022-12-10 19:43:34 +0100

    Tragated Month & Year Changes -m 2022 to 2023

--- Calender.py
+++ Calender.py
@@ -1,13 +1,13 @@
 # import module
 import calendar
    
-yy = 2022
-mm = 10
+yy = 2023
+mm = 01
    
 # Display the calendar for 1 month
 print(calendar.month(yy, mm)) # calendar() function to print calendar
 
 # using calendar function to print calendar of year
-print ("The calendar of year 2022 is :")
-print (calendar.calendar(2022, 2, 1, 6))
+print ("The calendar of year 2023 is :")
+print (calendar.calendar(2023, 2, 1, 6))
 
