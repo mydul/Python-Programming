771fb61473b719d2efa18ee0567e986b958e0400
Mydul Islam Rashed <muhurtoo@gmail.com>
2022-12-20 00:57:37 +0100

    Display the calendar for 1 month

--- Calender.py
+++ Calender.py
@@ -1,31 +1,13 @@
-"""
+
 # import module
 import calendar
    
-yy = 2023
-mm = 01
-   
 # Display the calendar for 1 month
-print(calendar.month(yy, mm)) # calendar() function to print calendar
+year = 2023
+month = 1
+print(calendar.month(year, month)) # calendar() function to print calendar
 
 # using calendar function to print calendar of year
-print ("The calendar of year 2023 is :")
-print (calendar.calendar(2023, 2, 1, 6))
+# print ("The calendar of year 2023 is :")
+# print (calendar.calendar(2023, 2, 1, 6)) #2,1,6 spacing
 
-"""
-
-import calendar
-
-# Prompt the user for a year
-year = int(input("Enter a year: "))
-
-# Create a calendar for the year
-c = calendar.Calendar()
-
-# Iterate over the months in the year
-for month in range(1, 13):
-    # Print the month name and year
-    print(calendar.month_name[month], year)
-    
-    # Print the calendar for the month
-    print(c.formatmonth(year, month))
