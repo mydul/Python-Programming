d389b1b34831f39d152e675d2763388f7e173347
Mydul Islam Rashed <muhurtoo@gmail.com>
2022-12-20 00:28:47 +0100

    New Calender style added in Calender.py

--- Calender.py
+++ Calender.py
@@ -1,3 +1,4 @@
+"""
 # import module
 import calendar
    
@@ -10,4 +11,20 @@ print(calendar.month(yy, mm)) # calendar() function to print calendar
 # using calendar function to print calendar of year
 print ("The calendar of year 2023 is :")
 print (calendar.calendar(2023, 2, 1, 6))
+"""
 
+import calendar
+
+# Prompt the user for a year
+year = int(input("Enter a year: "))
+
+# Create a calendar for the year
+c = calendar.Calendar()
+
+# Iterate over the months in the year
+for month in range(1, 13):
+    # Print the month name and year
+    print(calendar.month_name[month], year)
+    
+    # Print the calendar for the month
+    print(c.formatmonth(year, month))
